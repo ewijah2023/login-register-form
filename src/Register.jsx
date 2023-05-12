007d87965160883a8f5cd4cc37531267aac2c4b3
Ewijah <edgordon2020@gmail.com>
2023-05-12 11:47:28 -0400

    set up gh-pages

--- src/Register.jsx
+++ src/Register.jsx
@@ -3,7 +3,6 @@ import React, {useState } from "react";
 export const Register = (props) => {
     const [email, setEmail] = useState("");
     const [pass, setPass] = useState("");
-    const [name, setName] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
